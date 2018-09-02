d7a3ff6c8776ceb206c207d3ea85d682a64a9340
rambodrahmani <rambodrahmani@hypert-rahmani.localdomain>
2018-09-02 20:54:56 +0200

    Add Local Binary Pattern for texture classification project files.

--- sec5/LBP_similarity.py
+++ sec5/LBP_similarity.py
@@ -38,17 +38,92 @@
 # classifiers can be used for face recognition or texture analysis.
 ##
 
-import numpy as np
+import os
 import cv2
-import sys
-
-# load the image
-img = cv2.imread("car.jpeg")
-
-# display the loaded image
-cv2.imshow("Gray scale image Histogram", img)
+import glob
+import numpy as np
+from scipy.stats import itemfreq
+from skimage.feature import local_binary_pattern
 
 
+try:
+    # for Python2
+    from Tkinter import *
+except ImportError:
+    # for Python3
+    from tkinter import *
+
+import tkinter
+import tkinter.constants
+import tkinter.filedialog
+
+##
+# Loads the image at the given filename path. Resizes the loaded image.
+# Calculates the local binary pattern of the loaded image using uniform method.
+# Calculates and returns a feature vector for the loaded image based on the calculated LBP.
+##
+def imageToFeature(filename):
+	img = cv2.imread(filename, 0)
+	img = cv2.resize(img, (200, 100))
+	lbp = local_binary_pattern(img, 8*3, 3, method='uniform')
+	x = itemfreq(lbp.ravel())
+	feature = x[:, 1]/sum(x[:, 1])
+
+	return feature
+
+##
+# Calculates the distance (difference) between x and y.
+##
+def distance(x, y):
+	sumSq=0.0
+
+	for i in range(len(x)):
+		sumSq+=(x[i]-y[i])**2
+
+	return (sumSq**0.5)
+
+# similarity threshold
+closestDistance = 99999999
+
+# path of the most similar image found
+mostSimilarImage = ""
+
+print("This program will search for the given filename in the ./images folder using Local binary patterns.")
+
+# ask user to input search filename
+query = input("Enter filename: ")
+
+# get feature vector for the provided image
+queryFeature = imageToFeature(query)
+
+# loop through the pictures in the images folder
+for filename in glob.iglob('images/*.*'):
+	# for each image in the images folder
+	# calculate the feature vector using LBP
+	databaseFeature = imageToFeature(filename)
+
+	# calculate the difference between the feature vector of the loaded image and the
+	# current directory image
+	dist = distance(queryFeature,databaseFeature)
+
+	# if the difference is lower than the acceptable threshold
+	if dist < closestDistance:
+		# one of the the most similar image to the given one has been found
+		closestDistance = dist
+		mostSimilarImage = filename
+
+# show result
+print(mostSimilarImage)
+
+# load the image provided by the user
+queryImg = cv2.imread(query, 1)
+
+# loaded the most image that has been found
+mostSimilarImg = cv2.imread(mostSimilarImage, 1)
+
+# display both images
+cv2.imshow("Query ", queryImg)
+cv2.imshow("Most Similar ", mostSimilarImg)
 
 # wait for user input
 cv2.waitKey(0)
